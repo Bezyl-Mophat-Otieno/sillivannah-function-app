01b5663ddb51c9d320208385595cf45818a20848
Bezyl Mophat Otieno <bezylmophatotieno@gmail.com>
2025-07-25 15:00:33 +0300

    Refactors

--- src/functions/number-summation.js
+++ src/functions/number-summation.js
@@ -1,23 +1,23 @@
-const { app } = require('@azure/functions');
+const { app } = require('@azure/functions')
 
-app.http('number-summation', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const firstNumber = parseInt(request.query.firstNumber) || parseInt(request.body.firstNumber);
-        const secondNumber = parseInt(request.query.secondNumber) || parseInt(request.body.secondNumber);
-
-        if (!firstNumber || !secondNumber) {
-            context.log('Invalid input: firstNumber or secondNumber is missing or not a number.');
-            return { status: 400, body: 'Invalid input. Please provide two numbers.' };
+app.http("number-summmation", {
+    authLevel: "anonymous",
+    methods: ["POST", "GET"],
+    handler: async (request, context)=>{
+        const firstNumber = parseInt(request.query.firstNumber || request.body.firstNumber);
+        const secondNumber = parseInt(request.query.secondNumber || request.body.secondNumber);
+        if(!firstNumber || !secondNumber) {
+            return {
+                status: 400,
+                body: "Please provide both firstNumber and secondNumber"
+            }
         }
 
         const sum = firstNumber + secondNumber;
-        const responseMessage = `The sum of ${firstNumber} and ${secondNumber} is ${sum}.`;
-
-       
-        return {status: 200, body: responseMessage}
+        return {
+            status: 200,
+            body: `The sum of ${firstNumber} and ${secondNumber} is ${sum}`
+        }
     }
-});
+})
+
